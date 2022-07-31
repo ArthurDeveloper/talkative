420ce779c9ac6beaac2f0e05cffedc54998f9207
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-07-30 21:57:08 -0300

    feat: Added ability to send messages

--- src/components/Chat/index.jsx
+++ src/components/Chat/index.jsx
@@ -1,17 +1,51 @@
+import { useState, useRef, useEffect } from 'react';
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
 import './index.css';
 
 export default function Chat() {
-	const messages = new Array(30).fill(0).map((value, index) => {
+	const messagesRef = useRef(null);
+
+	const [messages, setMessages] = useState(new Array(30).fill(0).map((value, index) => {
 		return { 
 			sender: 'John Doe',
 			senderPicture: 'https://images.pexels.com/photos/834894/pexels-photo-834894.jpeg?auto=compress&cs=tinysrgb&w=600',
 			content: `Message ${index}`
 		}
-	});
+	}));
+
+	const handleMessageSent = (event) => {
+		if (event.key === 'Enter') {
+			if (event.target.value.length === 0) {
+				toast('Type something before sending!', {
+					position: "bottom-right",
+					autoClose: 5000,
+					hideProgressBar: false,
+					closeOnClick: true,
+					pauseOnHover: false,
+					draggable: false,
+					type: 'error'
+				});
+				return;
+			}
+
+			setMessages([...messages, {
+				sender: 'John Doe',
+				senderPicture: 'https://images.pexels.com/photos/834894/pexels-photo-834894.jpeg?auto=compress&cs=tinysrgb&w=600',
+				content: event.target.value
+			}]);
+
+			event.target.value = '';
+		}
+	}
+
+	useEffect(() => {
+		messagesRef.current.scroll({ top: messagesRef.current.scrollHeight });
+	}, [messages]);
 
 	return (
 		<div className="chat">
-			<div className="messages-wrapper">
+			<div className="messages-wrapper" ref={(e) => messagesRef.current = e}>
 				{messages.map((message, index) => {
 					return (
 						<div className="message" key={index}>
@@ -29,7 +63,13 @@ export default function Chat() {
 					)
 				})}
 			</div>
-			<input className="message-input" placeholder="Type your message..."/>		
+			<input
+				className="message-input"
+				placeholder="Type your message..."
+				onKeyPress={handleMessageSent}
+			/>		
+
+			<ToastContainer />
 		</div>
 	);
 }
