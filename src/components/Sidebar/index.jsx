6dc7493f55de508783f404d9cd5f84d27ec691b8
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-08-01 20:16:09 -0300

    feat: Added ability to create groups

--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -1,15 +1,50 @@
-import { useState } from 'react';
+import { useState, useRef, useEffect } from 'react';
+import ReactModal from 'react-modal';
 import './index.css';
 
 export default function Sidebar() {
-	const groups = new Array(5).fill(0).map((value, index) => {
+	const [groups, setGroups] = useState(new Array(5).fill(0).map((value, index) => {
 		return {
 			name: `Group ${index}`,
 			picture: 'https://images.pexels.com/photos/834894/pexels-photo-834894.jpeg?auto=compress&cs=tinysrgb&w=600'
 		}
-	});
+	}));
 
 	const [selectedGroup, setSelectedGroup] = useState(0);
+
+	const [modalOpen, setModalOpen] = useState(false);
+	const modalPictureDisplay = useRef(null);
+
+	const [modalGroupNameInput, setModalGroupNameInput] = useState('');
+	const [modalGroupPictureInput, setModalGroupPictureInput] = useState('');
+
+	const [modalFormSubmitWentWrong, setModalFormSubmitWentWrong] = useState(false);
+
+	const addGroup = (name, picture) => {
+		setGroups([...groups, { name, picture }]);
+	}
+
+	const handleGroupImageSelected = (event) => {
+		modalPictureDisplay.current.src = URL.createObjectURL(event.target.files[0]);
+		setModalGroupPictureInput(URL.createObjectURL(event.target.files[0]));
+	}
+
+	const handleGroupAddFormSubmit = (event) => {
+		event.preventDefault();
+		setModalFormSubmitWentWrong(false);
+	}
+
+	useEffect(() => {
+		if (!modalFormSubmitWentWrong) {
+			if (modalGroupNameInput.trim().length === 0) {
+				console.log('monke');
+				setModalFormSubmitWentWrong(true);
+				return;
+			}
+			setModalOpen(false);
+			addGroup(modalGroupNameInput, modalGroupPictureInput);
+		}
+	}, [modalFormSubmitWentWrong]);
 
 	return (
 		<aside>
@@ -25,7 +60,79 @@ export default function Sidebar() {
 							<div className="group-name">{value.name}</div>
 						</div>
 					)
-				})}	
+				})}
+			
+				<div className="add-group-btn-wrapper">
+					<button
+						className="add-group-btn"
+						onClick={() => setModalOpen(true)}
+					>
+						ADD
+					</button>
+				</div>
+			
+				<ReactModal
+					isOpen={modalOpen}
+					contentLabel="Add Group"
+					style={{
+						overlay: {
+							backgroundColor: '#00000088',
+							animation: modalOpen ? 
+								'add-group-modal-overlay-fade-in 200ms ease-in-out' : 'none'
+						},
+						content: {
+							backgroundColor: 'var(--background-secondary)',
+							border: 'none',
+							borderRadius: '0.5rem',
+							color: '#fff',
+							padding: '2.5rem',
+							animation: modalOpen ? 
+								'add-group-modal-content-fade-in 400ms cubic-bezier(.21,-0.07,0,1.24)' : 'none',
+						} 
+					}}
+				>
+					<h1>Add group</h1>
+
+					<form className="add-group-modal-form" onSubmit={handleGroupAddFormSubmit}>
+						<input
+							placeholder="Group name"
+							className= "add-group-modal-name-input"
+							value={modalGroupNameInput}
+							onChange={(e) => setModalGroupNameInput(e.target.value)}
+							style={{
+								animation: modalFormSubmitWentWrong ? 
+									"add-group-modal-name-input-error-shake 400ms ease-in-out" :
+									"none"
+							}}
+
+						/>
+						
+						<label
+							className="add-group-modal-picture-input"
+							for="add-group-modal-picture-input"
+						>
+							Upload picture (optional)
+						</label>
+						<input
+							type="file"
+							accept="image/*"
+							id="add-group-modal-picture-input"
+							onChange={(e) => handleGroupImageSelected(e)}
+						/>
+
+						<img
+							ref={(e) => modalPictureDisplay.current = e}
+							className="add-group-modal-picture-display"
+						/>
+
+						<button
+							type="submit"
+							className="add-group-modal-submit-btn"
+						>
+							ADD
+						</button>
+					</form>
+				</ReactModal>
 			</nav>
 		</aside>
 	)
