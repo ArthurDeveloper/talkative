c596e6a693804300803aa707f6eda035d6cbedca
Arthur Dev <73297053+ArthurDeveloper@users.noreply.github.com>
2022-08-07 14:26:03 -0300

    feat: Made sidebar responsive

--- src/App.jsx
+++ src/App.jsx
@@ -2,32 +2,82 @@ import Topbar from './components/Topbar';
 import Sidebar from './components/Sidebar';
 import Chat from './components/Chat';
 import { getAuth } from 'firebase/auth';
-import { useState, useEffect } from 'react';
+import { useState, useRef, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 export default function App() {
+	const pageRef = useRef(null);
+
 	const navigation = useNavigate();
+	
 	const [currentGroup, setCurrentGroup] = useState('');
 	const [loggedIn, setLoggedIn] = useState(true);
+;
+	const [touchStart, setTouchStart] = useState(0);
+	const [touchEnd, setTouchEnd] = useState(0);	
+	const [sidebarOpen, setSidebarOpen] = useState(true);
+
+	const adjustPageLayout = () => {
+		if (sidebarOpen) {
+			pageRef.current.classList.remove('page-mobile-sidebar-closed');
+		} else {
+			pageRef.current.classList.toggle('page-mobile-sidebar-closed');
+		}
+	}
+
+	const handleTouchStart = (e) => {
+		setTouchStart(e.targetTouches[0].clientX);
+	}
+
+	const handleTouchMove = (e) => {
+		setTouchEnd(e.targetTouches[0].clientX);
+	}
+
 	useEffect(() => {
 		const auth = getAuth();
 		auth.onAuthStateChanged((state) => {
 			setLoggedIn(state !== null);
 		});
+
+		window.addEventListener('resize', () => {
+			if (window.innerWidth < 768) {
+				setSidebarOpen(false);
+			}
+		});
 	}, []);
 
 	useEffect(() => {
 		if (!loggedIn) {
-			navigation('/login');
+			// navigation('/login');
 		}
 	}, [loggedIn]);
 
+	
+	useEffect(() => {
+		if (touchEnd - touchStart > 250) {
+			console.log('swipe left');
+			setSidebarOpen(true);
+		} else if (touchStart - touchEnd > 250) {
+			console.log('swipe right');
+			setSidebarOpen(false);
+		}
+	}, [touchEnd]);
+		
+	useEffect(() => {
+		adjustPageLayout();
+	}, [sidebarOpen]);
+
 	return (	
 		loggedIn &&
-		<div className="page">
+		<div 
+			className="page"
+			onTouchStart={handleTouchStart}
+			onTouchMove={handleTouchMove}
+			ref={(e) => pageRef.current = e}
+		>
 			<Topbar />
 
-			<Sidebar changeGroup={(newGroup) => setCurrentGroup(newGroup)}/>
+			<Sidebar changeGroup={(newGroup) => setCurrentGroup(newGroup)} open={sidebarOpen}/>
 			<Chat group={currentGroup} />	
 		</div>
 	);
--- src/components/Sidebar/index.jsx
+++ src/components/Sidebar/index.jsx
@@ -5,7 +5,7 @@ import { getStorage, ref, uploadBytes, getDownloadURL } from 'firebase/storage';
 import ReactModal from 'react-modal';
 import './index.css';
 
-export default function Sidebar({ changeGroup }) {
+export default function Sidebar({ changeGroup, open }) {
 	const [groups, setGroups] = useState([]);
 
 	const [selectedGroup, setSelectedGroup] = useState(0);
@@ -98,93 +98,99 @@ export default function Sidebar({ changeGroup }) {
 	}, [modalFormSubmitWentWrong]);
 
 	return (
-		<aside>
-			<nav className="groups">
-				{groups.map((value, index) => {
-					return (
-						<div
-							key={index}
-							className={`group-data ${selectedGroup === index ? 'selected' : ''}`}
-							onClick={() => setSelectedGroup(index)}
-						>
-							<img src={value.picture} className="group-picture" />
-							<div className="group-name">{value.name}</div>
-						</div>
-					)
-				})}
+		<>
+			<div className="dummy-background"  />
+			<aside style={{
+				width: open ? '100%' : '0',
+				transform: open ? 'translateX(0)' : 'translateX(-100%)',
+				transition: 'transform 300ms cubic-bezier(.17,.67,0,1.45)',	
+			}}>
+				<nav className="groups">
+					{groups.map((value, index) => {
+						return (
+							<div
+								key={index}
+								className={`group-data ${selectedGroup === index ? 'selected' : ''}`}
+								onClick={() => setSelectedGroup(index)}
+							>
+								<img src={value.picture} className="group-picture" />
+								<div className="group-name">{value.name}</div>
+							</div>
+						)
+					})}
 
-				<div className="add-group-btn-wrapper">
-					<button
-						className="add-group-btn"
-						onClick={() => setModalOpen(true)}
-					>
-						ADD
-					</button>
-				</div>
-			
-				<ReactModal
-					isOpen={modalOpen}
-					contentLabel="Add Group"
-					style={{
-						overlay: {
-							backgroundColor: '#00000088',
-							animation: modalOpen ? 
-								'add-group-modal-overlay-fade-in 200ms ease-in-out' : 'none'
-						},
-						content: {
-							backgroundColor: 'var(--background-secondary)',
-							border: 'none',
-							borderRadius: '0.5rem',
-							color: '#fff',
-							padding: '2.5rem',
-							animation: modalOpen ? 
-								'add-group-modal-content-fade-in 400ms cubic-bezier(.21,-0.07,0,1.24)' : 'none',
-						} 
-					}}
-				>
-					<h1>Add group</h1>
-
-					<form className="add-group-modal-form" onSubmit={handleGroupAddFormSubmit}>
-						<input
-							placeholder="Group name"
-							className= "add-group-modal-name-input"
-							value={modalGroupNameInput}
-							onChange={(e) => setModalGroupNameInput(e.target.value)}
-							style={{
-								animation: modalFormSubmitWentWrong ? 
-									"add-group-modal-name-input-error-shake 400ms ease-in-out" :
-									"none"
-							}}
-
-						/>
-						
-						<label
-							className="add-group-modal-picture-input"
-							for="add-group-modal-picture-input"
-						>
-							Upload picture (optional)
-						</label>
-						<input
-							type="file"
-							accept="image/*"
-							id="add-group-modal-picture-input"
-							onChange={(e) => handleGroupImageSelected(e)}
-						/>
-
-						<img
-							ref={(e) => modalPictureDisplay.current = e}
-							className="add-group-modal-picture-display"
-						/>
-
+					<div className="add-group-btn-wrapper">
 						<button
-							type="submit"
-							className="add-group-modal-submit-btn"
+							className="add-group-btn"
+							onClick={() => setModalOpen(true)}
 						>
 							ADD
 						</button>
-					</form>
-				</ReactModal>
-			</nav>
-		</aside>
+					</div>
+			
+					<ReactModal
+						isOpen={modalOpen}
+						contentLabel="Add Group"
+						style={{
+							overlay: {
+								backgroundColor: '#00000088',
+								animation: modalOpen ? 
+									'add-group-modal-overlay-fade-in 200ms ease-in-out' : 'none'
+							},
+							content: {
+								backgroundColor: 'var(--background-secondary)',
+								border: 'none',
+								borderRadius: '0.5rem',
+								color: '#fff',
+								padding: '2.5rem',
+								animation: modalOpen ? 
+									'add-group-modal-content-fade-in 400ms cubic-bezier(.21,-0.07,0,1.24)' : 'none',
+							} 
+						}}
+					>
+						<h1>Add group</h1>
+
+						<form className="add-group-modal-form" onSubmit={handleGroupAddFormSubmit}>
+							<input
+								placeholder="Group name"
+								className= "add-group-modal-name-input"
+								value={modalGroupNameInput}
+								onChange={(e) => setModalGroupNameInput(e.target.value)}
+								style={{
+									animation: modalFormSubmitWentWrong ? 
+										"add-group-modal-name-input-error-shake 400ms ease-in-out" :
+										"none"
+								}}
+							/>
+						
+							<label
+								className="add-group-modal-picture-input"
+								for="add-group-modal-picture-input"
+							>
+								Upload picture (optional)
+							</label>
+							<input
+								type="file"
+								accept="image/*"
+								id="add-group-modal-picture-input"
+								onChange={(e) => handleGroupImageSelected(e)}
+							/>
+
+							<img
+								ref={(e) => modalPictureDisplay.current = e}
+								className="add-group-modal-picture-display"
+							/>
+
+							<button
+								type="submit"
+								className="add-group-modal-submit-btn"
+							>
+								ADD
+							</button>
+						</form>
+					</ReactModal>
+				</nav>
+			</aside>
+		</>
 	)
 }
